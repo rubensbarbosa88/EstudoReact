2b2ea2220657cf8567a3b427fbc1198fdc7c190d
Rubens Barbosa Dos Santos <rubens.barbosa88@gmail.com>
2023-10-18 22:48:52 -0300

    custom hooks

--- 07_REQ_HTTP/http-react/src/App.jsx
+++ 07_REQ_HTTP/http-react/src/App.jsx
@@ -2,13 +2,31 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function App() {
+  const url = 'http://localhost:3000/products'
   const [products, setProducts] = useState([])
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
 
   async function getProducts() {
-    const url = 'http://localhost:3000/products'
     const response = await fetch(url)
     const data = await response.json()
     setProducts(data)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const payload = { name, price}
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+
+    await getProducts()
+    return await response.json()
   }
 
   useEffect(() => {
@@ -23,6 +41,21 @@ function App() {
           <li key={id}> {name} R$ {price}</li>
         )}
       </ul>
+
+      <hr />
+
+      <form onSubmit={handleSubmit}>
+        <label style={{display: 'block', margin: '20px 0'}}> 
+          <span style={{marginRight: '10px'}}>Nome</span>
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <label style={{display: 'block', marginBottom: '20px'}}> 
+          <span style={{marginRight: '10px'}}>Preço</span>
+          <input type="number" value={price} onChange={e => setPrice(e.target.value)} />
+        </label>
+
+        <button type='submit'> Criar </button>
+      </form>
     </>
   )
 }
