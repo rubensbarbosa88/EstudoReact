766bbd031043bad23e2fa76c3af39f14eb7a0815
Rubens Barbosa Dos Santos <rubens.barbosa88@gmail.com>
2023-10-30 23:52:57 -0300

    init miniblog

--- 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
 import styles from "./Register.module.css";
 
 const Register = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [error, setError] = useState('')
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    setError('')
+
+    if (form.password !== form.confirmPassword) {
+      setError('As senhas precisam ser iguais!')
+    }
+  }
 
   return (
     <div className={styles.register}>
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe suas histórias</p>
       
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span> Nome: </span>
           <input 
             type="text"
             name="displayName"
             placeholder="Nome do usuário"
+            value={form.name}
+            onChange={e => setForm({...form, name: e.target.value})}
             required />
         </label>
         <label>
@@ -22,6 +42,8 @@ const Register = () => {
             type="email"
             name="email"
             placeholder="E-mail do usuário"
+            value={form.email}
+            onChange={e => setForm({...form, email: e.target.value})}
             required />
         </label>
         <label>
@@ -30,6 +52,8 @@ const Register = () => {
             type="password"
             name="password"
             placeholder="Insira sua senha"
+            value={form.password}
+            onChange={e => setForm({...form, password: e.target.value})}
             required />
         </label>
         <label>
@@ -38,10 +62,13 @@ const Register = () => {
             type="password"
             name="confirmPassword"
             placeholder="Confirme a sua senha"
+            value={form.confirmPassword}
+            onChange={e => setForm({...form, confirmPassword: e.target.value})}
             required />
         </label>
 
         <button className="btn">Cadastrar</button>
+        {error && <p className="error">{error}</p>}
       </form>
     </div>
   );
